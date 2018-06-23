f486b6870bc520956068b1869eceba8d68dacf5c
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-06-23 16:36:49 +0200

    currency exchange service impl

---
+++ currency-exchange-service/src/main/java/com/vaxen/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,25 @@
+package com.vaxen.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		ExchangeValue exchangeValue = new ExchangeValue(100L, from, to, BigDecimal.valueOf(65));
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
+
+	}
+
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port)
+values (1001,'EUR','USD','12',0);
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port)
+values (1001,'EUR','GPB','24',0);
