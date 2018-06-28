6f7203de1cc5439e938e39cc10f8dbaba8112dfe
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-06-28 22:18:54 +0200

    impl register server/service discovery

--- currency-conversion-service/src/main/java/com/vaxen/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/vaxen/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-	
+
 	@Autowired
 	CurrencyExchangeServiceProxy proxy;
 
@@ -32,17 +32,19 @@ public class CurrencyConversionController {
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
-	
+
 	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
-//		Map<String, String> uriVariables = new HashMap<>();
-//		uriVariables.put("from", from);
-//		uriVariables.put("to", to);
-//		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
-//				"http://localhost:8000//currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
-//				uriVariables);
+		// Map<String, String> uriVariables = new HashMap<>();
+		// uriVariables.put("from", from);
+		// uriVariables.put("to", to);
+		// ResponseEntity<CurrencyConversionBean> responseEntity = new
+		// RestTemplate().getForEntity(
+		// "http://localhost:8000//currency-exchange/from/{from}/to/{to}",
+		// CurrencyConversionBean.class,
+		// uriVariables);
 
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
--- currency-conversion-service/src/main/java/com/vaxen/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/vaxen/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,14 @@ package com.vaxen.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@RibbonClient(name="currency-exchange-service")
 @EnableFeignClients("com/vaxen/microservices/currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
